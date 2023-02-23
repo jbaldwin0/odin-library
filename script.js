94513a1eae73ab6cb6986897238a228e4a095ad8
jbaldwin0 <jac0b67@ymail.com>
2023-02-22 20:48:05 -0600

    Define book object, create add to library method

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const myLibrary = [];
 
-function Book() {
-    // constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    // perform action
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
+/*
+function iterateThroughBooks() {
+    for (const book in myLibrary) {
+        return `${author}, ${title}, ${pages}, ${read}`;
+    }
+}
+*/
+const hobbit = new Book("The Hobbit", "Tolkien", "255", "Yes");
+addBookToLibrary(hobbit);
+console.log(myLibrary.pop());
