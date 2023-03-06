1695a4f5a55ac1083c60f5e8ab8e1a4dff680076
jbaldwin0 <jac0b67@ymail.com>
2023-03-06 17:36:01 -0600

    Set working code to push book jsons onto page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+var shelf = document.getElementById("shelf");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,14 +13,25 @@ function addBookToLibrary(book) {
 }
 
 function iterateThroughBooks(library) {
-
     for (let i = 0; i < library.length; i++) {
-        console.log(library.pop());
+        console.log(library[i]);
+        const book = `
+        <div class="books">
+            <p>${library[i].title}</p>
+        </div>
+        `;
+        shelf.insertAdjacentHTML("beforeend", book);
         // Perform iteration action
     }
 }
 
+// document.body.innerHTML = markup;
+// library[i].title;
 const hobbit = new Book("The Hobbit", "Tolkien", "255", "Yes");
+const pride = new Book("Pride and Prejudice", "Austen", "314", "Yes");
+const animal = new Book("Animal Farm", "Orwell", "157", "No");
 addBookToLibrary(hobbit);
-// console.log(myLibrary.pop());
+addBookToLibrary(pride);
+addBookToLibrary(animal);
+
 iterateThroughBooks(myLibrary);
